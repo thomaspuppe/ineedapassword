43d064ca929c4c6d1348464131ace92909bc2969
Thomas Puppe <info@thomaspuppe.de>
2018-05-11 21:14:03 +0200

    Generate and render passwords

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import pwgen from 'generate-password'
+ 
+const passwords = pwgen.generateMultiple(5, {
+    length: 10,
+    numbers: true
+})
 
-const title = 'Hello!!! My Minimal React Webpack Babel Setup';
+let passwordList = passwords.map(function(password, index){
+  return <li key={index}>{password}</li>
+})
+
+const title = 'Hello!!! Here are some passwords:'
 
 ReactDOM.render(
-  `<h1>${title}</h1>`,
+  <div><h1>{title}</h1>{passwordList}</div>,
   document.getElementById('app')
-);
+)
 
-module.hot.accept();
+module.hot.accept()
