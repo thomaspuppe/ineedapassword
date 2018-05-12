5143c2afd1da46ca8bbfb4271c91598c09a7aaee
Thomas Puppe <info@thomaspuppe.de>
2018-05-12 11:58:09 +0200

    Create components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import pwgen from 'generate-password'
- 
-const passwords = pwgen.generateMultiple(5, {
+
+
+function Headline(props) {
+  return <h1>{props.title}</h1>
+}
+
+function List(props) {
+  let itemList = props.items.map(function(item, index){
+    return <li key={index}>{item}</li>
+  })
+  return <ul>{itemList}</ul>
+}
+
+function App() {
+  const headline = 'Hello!!! Here are some passwords:'
+
+  const passwords = pwgen.generateMultiple(5, {
     length: 10,
     numbers: true
-})
+  })
 
-let passwordList = passwords.map(function(password, index){
-  return <li key={index}>{password}</li>
-})
-
-const title = 'Hello!!! Here are some passwords:'
+  return (
+    <div>
+      <Headline title={headline} />
+      <List items={passwords} />
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <div><h1>{title}</h1>{passwordList}</div>,
+  <App />,
   document.getElementById('app')
 )
 
