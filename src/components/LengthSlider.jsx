d951906152bd3a4a37822c67904b3001fc4ccd29
Thomas Puppe <info@thomaspuppe.de>
2018-05-28 13:08:46 +0200

    move state from LengthSlider up  to the app

--- src/components/LengthSlider.jsx
+++ src/components/LengthSlider.jsx
@@ -1,29 +1,12 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-export default class LengthSlider extends Component {
+const LengthSlider = ({passwordLength = 8, onLengthChange }) => 
+    <fieldset>
+    <input name="length" id="length" type="range" min="4" max="32" step="1"
+        value={passwordLength}
+        onChange={onLengthChange}/>
+        <span>{passwordLength}</span>
+    </fieldset>
 
-    constructor(props){
-        super(props)
-        this.state = {
-            passwordLength: 16
-        }
-        this.onLengthChange = this.onLengthChange.bind(this)
-    }
-
-    onLengthChange (evt) {
-        console.log(this.state.passwordLength)
-        this.setState({ passwordLength: evt.target.value }); 
-    }
-
-    render() {
-        return (
-            <fieldset>
-            <input name="length" id="length" type="range" min="4" max="32" step="1"
-                value={this.state.passwordLength}
-                onChange={this.onLengthChange}/>
-                <span>{this.state.passwordLength}</span>
-            </fieldset>
-        )
-    }
-}
+export default LengthSlider
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 import pwgen from 'generate-password'
@@ -7,20 +7,38 @@ import Headline from './components/common/Headline'
 import List from './components/common/List'
 import LengthSlider from './components/LengthSlider'
 
-function App() {
-  const headline = 'Hello!!! Here are some passwords:'
-  const passwords = pwgen.generateMultiple(5, {
-    length: 10,
-    numbers: true
-  })
+class App extends Component {
 
-  return (
+  constructor(props){
+    super(props)
+    this.state = {
+      passwordLength: 24
+    }
+    this.onPasswordLengthChange = this.onPasswordLengthChange.bind(this)
+  }
+
+  onPasswordLengthChange (passwordLength) {
+    // TODO: extract the value inside the component, not here!
+    this.setState({passwordLength: passwordLength.target.value})
+  }
+
+  render() {
+    const headline = 'Hello!!! Here are some passwords:'
+    const passwords = pwgen.generateMultiple(5, {
+      length: 10,
+      numbers: true
+    })
+
+    const { passwordLength } = this.state
+
+    return (
     <div>
       <Headline title={headline} />
-      <LengthSlider />
+      <LengthSlider passwordLength={passwordLength} onLengthChange={this.onPasswordLengthChange}/>
       <List items={passwords} />
     </div>
-  )
+    )
+  }
 }
 
 ReactDOM.render(
